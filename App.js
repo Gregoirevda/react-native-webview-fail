c787a56da0e2528755133e42512b61b308d23752
Van der Auwermeulen Grégoire <gregoirevandera@gmail.com>
2022-01-21 14:00:04 +0100

    running version failing

--- App.js
+++ App.js
@@ -1,18 +1,57 @@
-import React from 'react';
-import type {Node} from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {
+  Button,
+  ScrollView,
+  Text,
+  View,
+  useWindowDimensions,
+} from 'react-native';
+import WebView from 'react-native-webview';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <>
-      <ScrollView>
-        <View
-          style={{
-            backgroundColor: 'green',
-          }}>
-          <Text>Some text</Text>
-        </View>
-      </ScrollView>
-    </>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function HomeScreen({navigation}) {
+  return (
+    <ScrollView>
+      <WebView
+        source={{
+          html: '<h1> Hello world!</h1>',
+        }}
+        style={{
+          height: 50,
+          backgroundColor: 'yellow',
+          marginTop: 50,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'green',
+          height: 1000,
+        }}>
+        <Text style={{color: 'white'}}>Scroll Down!</Text>
+      </View>
+      <Button onPress={() => navigation.push('Settings')} title="Press" />
+    </ScrollView>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Settings Screen</Text>
+    </View>
   );
 }
